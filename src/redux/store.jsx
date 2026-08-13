c15f41b84d4c221d9d5acbaa11152ce959d2d721
bayramovh062-ui <bayramovh062@gmail.com>
2026-08-14 00:34:24 +0400

    fix in store import productSlice path

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import ProductReducer from './slice/ProductSlice'
+import ProductReducer from './slice/productSlice'
 import BasketReducer from './slice/basketSlice'
 
 export default configureStore({
